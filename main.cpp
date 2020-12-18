aed154dc714da8d74c59f5f9e9614123db463054
furokl <70150974+furokl@users.noreply.github.com>
2020-12-18 23:09:03 +0300

    комментарии, музыка

--- main.cpp
+++ main.cpp
@@ -1,15 +1,17 @@
-#include "SFML/Graphics.hpp"
+﻿#include "SFML/Graphics.hpp"
 #include "SFML/Audio.hpp"
 #include <random>
-#include <thread>	
-#include <chrono>	
 
 std::random_device rd;
 std::mt19937 mersenne(rd());
 
-const int H = 18;			// High for work field
-const int L = 10;			// Length for work field
-int field[H][L] = { 0 };
+const int H = 18;			// Высота поля
+const int L = 10;			// Длина поля
+
+// поле, в котором будут двигаться тетранимо
+int field[H][L];
+
+// запись фигур тетранимо
 int figures[7][4] =
 {
 	1, 3, 5, 7,		// |	
@@ -20,48 +22,58 @@ int figures[7][4] =
 	3, 5, 6, 7,		// L'
 	2, 3, 4, 5,		// []
 };
+
+// структура тетранимо
 struct Point {
 	int x, y;
 }	a[4], b[4];
-bool check(){
+
+bool check() {
 	for (int i = 0; i < 4; i++)
+		// если вышли за границы
 		if (a[i].x < 0 || a[i].x >= L || a[i].y >= H)	return 0;
+		// если встретились с полем ненулевого цвета
 		else if (field[a[i].y][a[i].x])					return 0;
 
 	return 1;
 }
-float bestresult = 0.f;			// max_score
-float score = 0.f;				// score
-float x_score = 1.f;			// multiplier
-int spawn = mersenne() % 7 + 1;	// spawn
-int dx = 0;						// move		
-int n = mersenne() % 7;			// type
-int color = mersenne() % 8 + 1;	// color
-int size = 20;					// size texture (20x20)
-bool rotate = false;			// rotate
-bool beginGame = true;			// for start
-bool pauseGame = false;			// pause
-bool looseGame = false;			// loose
-bool restartGame = false;		// restart
-bool muteMusic = false;			// mute music
-float timer = 0.f;
-float delay = 0.3f;				// delay
+
+float
+	bestresult = 0.f,			// лучший результат
+	score = 0.f,				// очки
+	x_score = 1.f,				// множитель
+	timer = 0.f,				// таймер
+	delay = 0.3f;				// задержка
+int
+	spawn = mersenne() % 7 + 1,	// точка появления
+	dx = 0,						// горизонтальное перемещение	
+	n = mersenne() % 7,			// тип тетранимо
+	color = mersenne() % 8 + 1,	// цвет тетранимо
+	size = 20;					// размер текстуры (20x20)
+bool
+	rotate = false,				// вращение
+	beginGame = true,			// начало игры
+	pauseGame = false,			// пауза
+	looseGame = false,			// поражение
+	restartGame = false,		// перезапуск
+	muteMusic = false;			// заглушить музыку
 
 int main()
 {
-	// window 
+	// создаем размер окна
 	sf::RenderWindow window(sf::VideoMode(240, 440), "*Tetris*");
 
+	// создаем таймер
 	sf::Clock clock;
 
-	// load texture
+	// загружаем текстуры
 	sf::Texture texture, backgroung;
 	texture.loadFromFile("C:\\Tetris\\Redist\\square160x20.png");
 	backgroung.loadFromFile("C:\\Tetris\\Redist\\fon.png");
 
 	sf::Sprite square(texture), sprite_background(backgroung);
 
-	// loaf font
+	// загружаем шрифты
 	sf::Font font;
 	font.loadFromFile("C:\\Tetris\\Redist\\SingleDayRegular.ttf");
 
@@ -70,7 +82,8 @@ int main()
 	text_loose.setFont(font);
 	text_restart.setFont(font);
 
-	// load sound fx
+	// загружаем аудио
+		// звук
 	sf::SoundBuffer sample;
 	sample.loadFromFile("C:\\Tetris\\Redist\\pong.wav");
 
@@ -78,85 +91,137 @@ int main()
 	pong.setBuffer(sample);
 	pong.setVolume(20);
 
+		// основная музыка
 	sf::Music music;
-	music.openFromFile("C:\\Tetris\\Redist\\sosnin.ogg");
+	music.openFromFile("C:\\Tetris\\Redist\\lofi.ogg");
 	music.play();
 	music.setLoop(true);
 
+		// музыка при поражении
 	sf::Music looseMusic;
-	looseMusic.openFromFile("C:\\Tetris\\Redist\\SektorGaza-Bomj.ogg");
+	looseMusic.openFromFile("C:\\Tetris\\Redist\\Deadmau5.ogg");
 
-
+	// пока окно открыто
 	while (window.isOpen()) {
 		float time = clock.getElapsedTime().asSeconds();
 		clock.restart();
 		timer += time;
 
+		// создаем событие
 		sf::Event event;
 		while (window.pollEvent(event)) {
+
+			// если пользователь закрыл окно
 			if (event.type == sf::Event::Closed)
 				window.close();
+
+			// если нажали клавишу
 			if (event.type == sf::Event::KeyPressed)
 				switch (event.key.code) {
-				case sf::Keyboard::Escape:	window.close();			break;
-				case sf::Keyboard::Left:	dx = -1;				break;
-				case sf::Keyboard::Right:	dx = 1;					break;
-				case sf::Keyboard::Up:		rotate = true;			break;
-				case sf::Keyboard::Down:	delay = 0.05f;			break;
-				case sf::Keyboard::A:		dx = -1;				break;
-				case sf::Keyboard::D:		dx = 1;					break;
-				case sf::Keyboard::W:		rotate = true;			break;
-				case sf::Keyboard::S:		delay = 0.05f;			break;
-				case sf::Keyboard::R:		restartGame = true;		break;
-				case sf::Keyboard::M:		
-					muteMusic ? muteMusic = 0 : muteMusic = 1; 		break;
+				case sf::Keyboard::Escape:	
+					window.close();	
+					break;
+				case sf::Keyboard::Left:
+					dx = -1;		
+					break;
+				case sf::Keyboard::Right:
+					dx = 1;			
+					break;
+				case sf::Keyboard::Up:
+					rotate = true;		
+					break;
+				case sf::Keyboard::Down:
+					delay = 0.05f;		
+					break;
+				case sf::Keyboard::A:
+					dx = -1;			
+					break;
+				case sf::Keyboard::D:	
+					dx = 1;				
+					break;
+				case sf::Keyboard::W:	
+					rotate = true;		
+					break;
+				case sf::Keyboard::S:	
+					delay = 0.05f;		
+					break;
+				case sf::Keyboard::R:	
+					restartGame = true;	
+					break;
+				case sf::Keyboard::M:
+					(muteMusic) ? 
+						muteMusic = false: 
+						muteMusic = true; 	
+					break;
 				}
+
+			// если клавиша отжата
 			if (event.type == sf::Event::KeyReleased)
 				switch (event.key.code) {
-				case sf::Keyboard::Down:	delay = 0.3f;	break;
-				case sf::Keyboard::S:		delay = 0.3f;	break;	
+				case sf::Keyboard::Down:	
+					delay = 0.3f;
+					break;
+				case sf::Keyboard::S:
+					delay = 0.3f;
+					break;
 				}
+
+			// если пользователь свернул окно
 			if (event.type == sf::Event::LostFocus) {
 				pauseGame = true;
 				if (!looseGame)
 					if (!muteMusic)
 						music.setVolume(3);
 			}
+
+			// если пользователь взаимодействует с окном
 			if (event.type == sf::Event::GainedFocus) {
 				pauseGame = false;
 				if (!looseGame)
 					if (!muteMusic)
 						music.setVolume(15);
 			}
-
 		}
+
+		// отрисовка текста
 		window.clear(sf::Color::White);
 		window.draw(sprite_background);
 		window.draw(text_score);
 		window.draw(text_loose);
 		window.draw(text_restart);
 
-		// restart game
+		// перезапустить игру
 		if (restartGame) {
 			restartGame = false;
 			if (looseGame) {
+				// убираем текст рестарта
 				text_restart.setString("");
+
+				// фиксируем не поражение
 				looseGame = false;
+
+				// возвращаем полю нулевое значение цвета
 				for (int i = 0; i < H; i++)
 					for (int j = 0; j < L; j++)
 						field[i][j] = 0;
-				if(!muteMusic){
-					music.setVolume(15);	
+
+				// музыка заглушена?
+				if (!muteMusic) {
+					music.setVolume(15);
 					looseMusic.stop();
 				}
+
+				// перезаписываем надпись "рестарт" на лучший результат
 				text_loose.setFillColor(sf::Color::White);
 				text_loose.setString("" + std::to_string(bestresult));
+
+				// сброс счетчика очков
 				x_score = 1.f;
 				score = 0;
 			}
 		}
 
-		// field render
+		// отрисовка поля
 		for (int i = 0; i < H; i++)
 			for (int j = 0; j < L; j++) {
 				if (field[i][j] == 0) continue;
@@ -165,7 +230,8 @@ int main()
 				square.move(20, 20);
 				window.draw(square);
 			}
-		// detail render
+
+		// отрисовка фигуры
 		for (int i = 0; i < 4; i++) {
 			square.setTextureRect(sf::IntRect((color - 1) * size, 0, size, size));
 			square.setPosition(a[i].x * 20, a[i].y * 20);
@@ -173,13 +239,16 @@ int main()
 			window.draw(square);
 		}
 
-		// when you dont lose
+		// если не поражение
 		if (!looseGame) {
-			// when window is only open
+			// если не пауза
 			if (!pauseGame) {
-				!muteMusic ? music.setVolume(15) : music.setVolume(0);
+				// устанавливаем громкость музыки
+				(!muteMusic) ?
+					music.setVolume(15):
+					music.setVolume(0);
 
-				// first figure
+				// генерируем первую фигуру
 				if (beginGame) {
 					beginGame = false;
 					n = mersenne() % 7;
@@ -190,26 +259,34 @@ int main()
 					}
 				}
 
-				// move tetranimo
-					// right + left
+				// горизонтальное перемещение
 				for (int i = 0; i < 4; i++) {
 					b[i] = a[i];
 					a[i].x += dx;
 				}
+
+				// если противоречит условию, возвращаемся
 				if (!check()) {
 					for (int i = 0; i < 4; i++)
 						a[i] = b[i];
 				}
-				// down
-				if (timer > delay) {
+
+				// движение вниз
+				if (timer > delay) {	// если таймер превысил значение задержки
+					// сохраняем предыдущее значение и спускаемся вниз
 					for (int i = 0; i < 4; i++) {
 						b[i] = a[i];
 						a[i].y += 1;
 					}
+
+					// если противоречит условию
 					if (!check()) {
+						// фиксируем фигуру в поле
 						for (int i = 0; i < 4; i++) {
 							field[b[i].y][b[i].x] = color;
-							if (b[i].y <= 3) {
+
+							// если фигуры достигли границы (проигрыш)
+							if (b[i].y <= 3) {		
 								text_restart.setPosition(50, -1);
 								text_restart.setFillColor(sf::Color::Black);
 								text_restart.setCharacterSize(16);
@@ -218,16 +295,26 @@ int main()
 								text_loose.setString("YOU LOSE :(");
 								text_loose.setFillColor(sf::Color::Red);
 								text_loose.setCharacterSize(42);
+
+								// музыка, при поражении
 								if (!muteMusic) {
 									looseMusic.play();
 									looseMusic.setVolume(15);
 								}
+
+								// глушим основную музыку
 								music.setVolume(0);
+
+								// фиксируем поражение
 								looseGame = true;
+
+								// фиксируем лучший результат
 								if (bestresult < score)
 									bestresult = score;
 							}
 						}
+
+						// генерируем новую фигуру
 						spawn = mersenne() % 7 + 1;
 						color = mersenne() % 8 + 1;
 						n = mersenne() % 7;
@@ -237,28 +324,41 @@ int main()
 							a[i].x += spawn;
 						}
 					}
+
+					// сброс таймера
 					timer = 0;
 				}
 
-				// rotate tetranimo			
-				if (n == 6) rotate = false;			// block for type n = 6 []
+				// вращение фигуры		
+				if (n == 6) rotate = false;	// блокируем вращение для квадрата
 				if (rotate) {
-					Point p = a[1];
+					Point p = a[1];			// создаем центр вращения
+
+					/*	Вращение по формуле:
+					*	X = x0 + (x − x0) * cos⁡(a) − (y − y0) * sin⁡(a);
+					*	Y = y0 + (y − y0) * cos⁡(a) + (x − x0) * sin⁡(a);
+					*
+					*	при a = 90 -> sin(90) = 1, cos(90) = 0.
+					*	X = x0 − (y − y0)
+					*	Y = y0 + (x − x0)
+					*/
 					for (int i = 0; i < 4; i++) {
-						int x = a[i].y - p.y;		// y - y0
-						int y = a[i].x - p.x;		// x - x0
-						a[i].x = p.x - x;
-						a[i].y = p.y + y;
+						int x = a[i].y - p.y;	// y - y0
+						int y = a[i].x - p.x;	// x - x0
+						a[i].x = p.x - x;		// x0 − (y − y0)
+						a[i].y = p.y + y;		// y0 + (x − x0)
 					}
+
+					// если противоречит условию, возвращаем как было
 					if (!check()) {
 						for (int i = 0; i < 4; i++)
 							a[i] = b[i];
 					}
 				}
 
-				// delete tetranimo
+				// ломаем линию
 				int k = H - 1;
-				int temp1 = 0;
+				int temp1 = 0; // счетчик сломанных линий
 				for (int i = k; i > 0; i--) {
 					int temp = 0;
 					for (int j = 0; j < L; j++) {
@@ -271,36 +371,45 @@ int main()
 					if (temp == L)
 						temp1++;
 				}
+
+				// присваивание очков
 				switch (temp1) {
 				case 1:
 					pong.play();
 					x_score += 0.1111f;
-					score += x_score;		break;
+					score += x_score;	
+					break;
 				case 2:
 					pong.play();
 					x_score += 0.3333f;
-					score += x_score * 2;	break;
+					score += x_score * 2;
+					break;
 				case 3:
 					pong.play();
 					x_score += 0.5555f;
-					score += x_score * 3;	break;
-				case 4:	
+					score += x_score * 3;
+					break;
+				case 4:
 					pong.play();
 					x_score += 0.7777f;
-					score += x_score * 4;	break;
+					score += x_score * 4;
+					break;
 				}
 
+				// сброс значений
 				temp1 = 0;
 				dx = 0;
 				rotate = 0;
-			}
-			// end if pause
-		}
-		// end if lose
+			}// конец если не пауза
+		}// конец если не поражение
+		
+		// отображение очков
 		text_score.setPosition(55, 392);
 		text_score.setString("Score: " + std::to_string(score));
 		text_score.setCharacterSize(24);
+
+		// вывод на экран
 		window.display();
-	}
+	}// конец пока окно открыто
 	return 0;
 }
